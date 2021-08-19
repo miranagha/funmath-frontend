2544287f990c1f6f0e480b5e50aaeeb9570d9679
Miran Agha <magha001@gold.ac.uk>
2021-08-19 17:20:38 +0100

    debugging

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 // export const SERVER_LOGIN='http://localhost:5000'
-// export const SERVER_LOGIN='https://funmathapp.herokuapp.com/'
+export const SERVER_LOGIN='https://funmathapp.herokuapp.com'
 
   
-export  const BASE_URL="https://funmathapp.herokuapp.com"
+// export  const BASE_URL="https://funmathapp.herokuapp.com"
